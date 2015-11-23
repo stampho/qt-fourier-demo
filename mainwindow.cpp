cf31a9c8298afb4c43c27e2db5d13c5d83295433
Peter Varga <pvarga@inf.u-szeged.hu>
2015-11-23 20:30:25 +0100

    Tweak GUI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,9 +16,10 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     m_progress->setWindowModality(Qt::WindowModal);
-    m_progress->setRange(0, 120);
+    m_progress->setRange(0, 130);
     m_progress->setCancelButton(0);
     m_progress->setLabelText("Working on Fourier Transformation...");
+    m_progress->cancel();
 
     for (int i = 0; i < FT::FTTYPECOUNT; ++i) {
         QString text;
@@ -93,74 +94,75 @@ void MainWindow::startCompare()
     ui->originalImageMod->setPixmap(pixmap);
     ui->originalImageMod->setFixedSize(pixmap.size());
 
-    FT *fourierRef = FT::createFT((FT::FTType)ui->refFtCombo->currentIndex(), &image);
     m_progress->setValue(10);
+    FT *fourierRef = FT::createFT((FT::FTType)ui->refFtCombo->currentIndex(), &image);
+    m_progress->setValue(20);
 
     FImage magnitudeImageRef = fourierRef->magnitudeImage();
     QPixmap magnitudePixmapRef = QPixmap::fromImage(magnitudeImageRef);
     ui->magnitudeImageRef->setPixmap(magnitudePixmapRef);
     ui->magnitudeImageRef->setFixedSize(magnitudePixmapRef.size());
-    m_progress->setValue(20);
+    m_progress->setValue(30);
 
     FImage recMagnitudeImageRef = fourierRef->reconstructFromMagnitude();
     QPixmap recMagnitudePixmapRef = QPixmap::fromImage(recMagnitudeImageRef);
     ui->recMagnitudeImageRef->setPixmap(recMagnitudePixmapRef);
     ui->recMagnitudeImageRef->setFixedSize(recMagnitudePixmapRef.size());
-    m_progress->setValue(30);
+    m_progress->setValue(40);
 
     FImage phaseImageRef = fourierRef->phaseImage();
     QPixmap phasePixmapRef = QPixmap::fromImage(phaseImageRef);
     ui->phaseImageRef->setPixmap(phasePixmapRef);
     ui->phaseImageRef->setFixedSize(phasePixmapRef.size());
-    m_progress->setValue(40);
+    m_progress->setValue(50);
 
     FImage recPhaseImageRef = fourierRef->reconstructFromPhase();
     QPixmap recPhasePixmapRef = QPixmap::fromImage(recPhaseImageRef);
     ui->recPhaseImageRef->setPixmap(recPhasePixmapRef);
     ui->recPhaseImageRef->setFixedSize(recPhasePixmapRef.size());
-    m_progress->setValue(50);
+    m_progress->setValue(60);
 
     FImage recOriginalImageRef = fourierRef->reconstructOriginalImage();
     QPixmap recOriginalPixmapRef = QPixmap::fromImage(recOriginalImageRef);
     ui->recOriginalImageRef->setPixmap(recOriginalPixmapRef);
     ui->recOriginalImageRef->setFixedSize(recOriginalPixmapRef.size());
-    m_progress->setValue(60);
+    m_progress->setValue(70);
 
     delete fourierRef;
 
 
     FT *fourierMod = FT::createFT((FT::FTType)ui->modFtCombo->currentIndex(), &image);
-    m_progress->setValue(70);
+    m_progress->setValue(80);
 
     FImage magnitudeImageMod = fourierMod->magnitudeImage();
     QPixmap magnitudePixmapMod = QPixmap::fromImage(magnitudeImageMod);
     ui->magnitudeImageMod->setPixmap(magnitudePixmapMod);
     ui->magnitudeImageMod->setFixedSize(magnitudePixmapMod.size());
-    m_progress->setValue(80);
+    m_progress->setValue(90);
 
     FImage recMagnitudeImageMod = fourierMod->reconstructFromMagnitude();
     QPixmap recMagnitudePixmapMod = QPixmap::fromImage(recMagnitudeImageMod);
     ui->recMagnitudeImageMod->setPixmap(recMagnitudePixmapMod);
     ui->recMagnitudeImageMod->setFixedSize(recMagnitudePixmapMod.size());
-    m_progress->setValue(90);
+    m_progress->setValue(100);
 
     FImage phaseImageMod = fourierMod->phaseImage();
     QPixmap phasePixmapMod = QPixmap::fromImage(phaseImageMod);
     ui->phaseImageMod->setPixmap(phasePixmapMod);
     ui->phaseImageMod->setFixedSize(phasePixmapMod.size());
-    m_progress->setValue(100);
+    m_progress->setValue(110);
 
     FImage recPhaseImageMod = fourierMod->reconstructFromPhase();
     QPixmap recPhasePixmapMod = QPixmap::fromImage(recPhaseImageMod);
     ui->recPhaseImageMod->setPixmap(recPhasePixmapMod);
     ui->recPhaseImageMod->setFixedSize(recPhasePixmapMod.size());
-    m_progress->setValue(110);
+    m_progress->setValue(120);
 
     FImage recOriginalImageMod = fourierMod->reconstructOriginalImage();
     QPixmap recOriginalPixmapMod = QPixmap::fromImage(recOriginalImageMod);
     ui->recOriginalImageMod->setPixmap(recOriginalPixmapMod);
     ui->recOriginalImageMod->setFixedSize(recOriginalPixmapMod.size());
-    m_progress->setValue(120);
+    m_progress->setValue(130);
 
     delete fourierMod;
 }
